593d2741c5cc990dc387a312c3f8b2047ff4aafd
快乐西瓜 <106435511+happy-watermelon@users.noreply.github.com>
2022-06-01 23:21:06 +0800

    Add files via upload

---
+++ 黑马练习专用/input.java
@@ -0,0 +1,12 @@
+//导包
+import java.util.Scanner;
+public class input{
+	public static void main(String[] args){
+		//创建对象
+		Scanner sc=new Scanner(System.in);
+		//接收数据,接收键盘录入
+		int a=sc.nextInt();
+		//打印输出结果
+		System.out.println(a);
+	}
+}
---
+++ 黑马练习专用/teacherinformation.java
@@ -0,0 +1,11 @@
+//输入老师的信息
+public class teacherinformation{
+	public static void main(String args[]){
+		String name="黑马蔡徐坤";
+		int age=18;
+		float height=180.2f;
+		boolean single=true;
+		char gender='男';
+		System.out.println(name+"的年龄是"+age+"岁,身高"+height+"cm,性别"+gender+"是否单身:"+single);
+	}
+}
