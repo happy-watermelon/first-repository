06d564a1f33abb4d7138c2f8bdf8442b9a9d7b26
快乐西瓜 <106435511+happy-watermelon@users.noreply.github.com>
2022-06-26 23:33:47 +0800

    Add files via upload

---
+++ 黑马练习专用/user.java
@@ -0,0 +1,104 @@
+package com.watermelon.demo03;
+
+public class user {
+    private String id;
+    private String password;
+    private String email;
+    private String sex;
+    private int age;
+
+    public user() {
+    }
+
+    public user(String id, String password, String email, String sex, int age) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * 设置
+     * @param sex
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "user{id = " + id + ", password = " + password + ", email = " + email + ", sex = " + sex + ", age = " + age + "}";
+    }
+}
---
+++ 黑马练习专用/userTest.java
@@ -0,0 +1,8 @@
+package com.watermelon.demo03;
+
+public class userTest {
+    public static void main(String[] args) {
+
+
+    }
+}
